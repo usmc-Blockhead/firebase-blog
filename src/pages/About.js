d67cf3bf513b75a4a769842c500d8a5df68179b5
Christa <jaysplaypen@gmail.com>
2023-09-04 23:17:19 -0700

    project setup

---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container padding">
+        <div className="col-md-12">
+            <div className="row mx-0">
+                <h1>
+                    About Me!
+                </h1>
+                <p>
+                    Hello, my name is <strong>Christa Cooper</strong> and I am a full-stack web developer. I am passionate about creating user-friendly and responsive web applications. I am a quick learner and a team player, and I am always looking for new challenges and opportunities to grow as a developer. I am currently seeking a junior role in full-stack or front-end development, where I can apply my skills and knowledge to create innovative and user-friendly solutions for real-world problems.
+                </p>
+                <p>
+                    I am a software engineer graduate with over 20 years of experience in business-based customer service and finance. I recently completed a one-year online boot camp at NuCamp Coding Bootcamp, where I learned the fundamentals of web and mobile development, data structures and algorithms, backend, SQL, and DevOps with Python. I also earned multiple certifications and honors for my projects and assignments.
+                </p>
+
+                <p>
+                    I am passionate about computers and software engineering, and I decided to make a career change after discovering the opportunities and challenges that this field offers. I have a strong work ethic, a customer-oriented mindset, and a collaborative attitude. I am eager to apply my skills and knowledge to create innovative and user-friendly solutions for real-world problems. I am currently seeking a junior role in full-stack or front-end development, where I can grow and learn from experienced professionals.
+                </p>
+            </div>
+        </div>
+    </div>
+    )
+};
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container padding">
+        <div className="col-md-12">
+            <div className="row mx-0">
+                <h1>
+                    Welcome to my_Blog!
+                </h1>
+                <p>
+                    Come with me on a journey while I build this blog to include in my portfolio. I will be adding new features and functionality as we continue along. I will be using React, React Router, React Bootstrap, and Firebase for the front-end, and Node.js, Express, MongoDB Atlas and Firebase for the back-end. I will also be using React Context API for state management, and React Hooks for functional components. I will be using React Router for routing, and React Bootstrap for styling. I will be using Firebase for authentication, and Firestore for the database. I will be using Firebase Storage for storing images, and Firebase Hosting for hosting the application. I will be using React Toastify for displaying toast messages, and React Tag Input for tags. I will be using React Paginate for pagination, and React Helmet for SEO. I will be using React Quill for the text editor, and React Icons for icons. I will be using React Share for social media sharing, and React Chartjs for charts.
+                </p>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Home;
