01a0b70e02fa902050815bd715e51bf898087f10
Christa <jaysplaypen@gmail.com>
2023-11-30 14:34:38 -0800

    fix compiling error

--- src/pages/AddEditBlog.js
+++ src/pages/AddEditBlog.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import ReactTagInput from "@pathofdev/react-tag-input";
 import "@pathofdev/react-tag-input/build/index.css";
 import { db, storage } from "../firebase";
